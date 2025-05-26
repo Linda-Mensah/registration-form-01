94dbfe6c665eccdad2e419055fd427c4410851d3
Linda-Mensah <lindaakmensah@gmail.com>
2025-05-26 21:07:47 +0000

    first commit

---
+++ registration-form-01.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("regForm");
+const messageContainer = document.getElementById("messageContainer");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+
+  messageContainer.textContent = "";
+  messageContainer.className = "message";
+
+  if (password.length < 8) {
+    messageContainer.textContent = "Have you tried 8 characters?";
+    messageContainer.classList.add("error");
+  } else if (password !== confirmPassword) {
+    messageContainer.textContent = "Nope. Passwords don't match.";
+    messageContainer.classList.add("error");
+  } else {
+    messageContainer.textContent = "High five! You're duly registered!";
+    messageContainer.classList.add("success");
+    form.reset();
+  }
+});
